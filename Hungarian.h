a8c23e7ec41d10246a08fdb32b062bcf30526bd4
Ismail Sunni <imajimatika@gmail.com>
2021-12-28 10:55:04 +0100

    Remove using namespace std

--- Hungarian.h
+++ Hungarian.h
@@ -1,13 +1,13 @@
 ///////////////////////////////////////////////////////////////////////////////
 // Hungarian.h: Header file for Class HungarianAlgorithm.
-// 
+//
 // This is a C++ wrapper with slight modification of a hungarian algorithm implementation by Markus Buehren.
 // The original implementation is a few mex-functions for use in MATLAB, found here:
 // http://www.mathworks.com/matlabcentral/fileexchange/6543-functions-for-the-rectangular-assignment-problem
-// 
+//
 // Both this code and the orignal code are published under the BSD license.
 // by Cong Ma, 2016
-// 
+//
 
 #ifndef HUNGARIAN_H
 #define HUNGARIAN_H
@@ -15,7 +15,7 @@
 #include <iostream>
 #include <vector>
 
-using namespace std;
+// using namespace std;
 
 
 class HungarianAlgorithm
@@ -23,7 +23,7 @@ class HungarianAlgorithm
 public:
 	HungarianAlgorithm();
 	~HungarianAlgorithm();
-	double Solve(vector <vector<double> >& DistMatrix, vector<int>& Assignment);
+	double Solve(std::vector <std::vector<double> >& DistMatrix, std::vector<int>& Assignment);
 
 private:
 	void assignmentoptimal(int *assignment, double *cost, double *distMatrix, int nOfRows, int nOfColumns);
